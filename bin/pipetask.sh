5814376b868b9dc8b6bf3ea38216c1fdcf4a314b
Kian-Tat Lim <ktl@slac.stanford.edu>
2021-04-29 10:09:17 -0700

    Add a generic pipetask script.

---
+++ bin/pipetask.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# Load the software and environment configuration
+source "/opt/lsst/software/stack/loadLSST.bash"
+if [[ -n $EUPS_TAG ]]; then
+    setup -t "$EUPS_TAG" lsst_distrib
+else
+    setup lsst_distrib
+fi
+
+OUTPUT_COLLECTION="u/ocps/$JOB_ID"
+
+# RUN_OPTIONS includes input collections and config overrides
+pipetask run -p "$PIPELINE_URL" \
+    -b "$BUTLER_REPO" \
+    -o "$OUTPUT_COLLECTION" \
+    $RUN_OPTIONS \
+    --output-run "${OUTPUT_COLLECTION}/run" \
+    -d "$DATA_QUERY"
+
+paths=$(butler query-datasets --collections $OUTPUT_COLLECTION/run $BUTLER_REPO "metricvalue_*" | sed -e 's|.*file://([^ ]*) .*|\1|')
+
+cp $paths $JOB_OUTPUT_DIR
