4bfc38f117f37a928d06e2098695aae1b68d401f
Simon Krughoff <krughoff@lsst.org>
2021-05-07 18:11:28 +0000

    Comment out butler

--- bin/simon_pipetask.sh
+++ bin/simon_pipetask.sh
@@ -23,4 +23,4 @@ pipetask run -t "$PIPELINE_TASK_CLASS" \
     -d "$DATA_QUERY"
 
 # OUTPUT_GLOB matches dataset types that should be made into outputs
-butler retrieve-artifacts --collections $OUTPUT_COLLECTION/run --dataset-type "$OUTPUT_GLOB" $BUTLER_REPO $JOB_OUTPUT_DIR
+# butler retrieve-artifacts --collections $OUTPUT_COLLECTION/run --dataset-type "$OUTPUT_GLOB" $BUTLER_REPO $JOB_OUTPUT_DIR
